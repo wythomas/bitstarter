774f44c946f13a6324ec5f6012ffac14bff6ecee
W Yee <waikuenyee@yahoo.com>
2013-07-10 21:11:37 +0000

    Updated web.js with read from file

--- web.js
+++ web.js
@@ -2,9 +2,28 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
+
+
+var fs = require('fs');
+var fname = "index.html";
+var iresults = function(fname){
+   return (fs.readFileSync(fname))
+};
+
+var ibuffer = new Buffer(30);
+
+//ibuffer = fs.readFileSync('index.html');
+//console.log("DEBUG");
+//console.log(ibuffer.toString());
+
+app.get('/', function(request, response){
+  ibuffer = fs.readFileSync('index.html');
+  response.send(ibuffer.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
