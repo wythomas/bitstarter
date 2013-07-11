c9ee6a2eaaebebcfca57f38c11359996ec430583
W Yee <waikuenyee@yahoo.com>
2013-07-11 16:16:21 +0000

    Updated grader to take a url

--- grader.js
+++ grader.js
@@ -22,10 +22,14 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
+var util = require('util')
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT= "checks.json";
+var URLFILE_DEFAULT = "http://fierce-reaches-1073.herokuapp.com"
+var CSVFILE_DEFAULT = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +39,51 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertURLExists = function(inURL) {
+   var instr = inURL.toString();
+   return instr;
+};
+
+/*
+//first concept based on marketresearch.js
+var getURLSource= function(inURL){
+   var csvfile = 'csvfile.html';
+   var response2console = buildfn(csvfile);
+   rest.get(inURL.toString()).on('complete', response2console);
+   return csvfile;
+};
+
+var buildfn =function(csvfile){
+   var response2console = function(result, response){
+     if (result instanceof Error) {
+        console.error('Error: ' + util.format(response.message));
+     }else{
+        console.error("Wrote %s", csvfile);
+        fs.writeFileSync(csvfile, result);
+     }
+   };   
+   return response2console;
+};
+ */
+var checkUrlFile = function(inURL, checksfile) {
+    var uout = {};
+    var csvfile = 'csvfile.html';
+    rest.get(inURL).on('complete', function(result, response){
+      if (result instanceof Error) {
+        console.error('Error: ' + util.format(response.message));
+      }else{
+        console.error("Wrote %s", csvfile);
+        fs.writeFileSync(csvfile, result);
+        uout = checkHtmlFile('csvfile.html', checksfile);
+      }
+    });
+//    if(uout.isEmpty){
+//      console.log('empty out');
+//    } else { console.log('not empty');
+    console.log('before out', uout);
+    return uout;
+};   
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -62,13 +111,27 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
+console.log("main mdule");
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url', clone(assertURLExists), URLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+//    if (program.file){
+//        var checkJson = checkHtmlFile(program.file, program.checks);
+//    }
+console.log(program.url);
+      if (program.url != URLFILE_DEFAULT) {
+         var checkUrl = checkUrlFile(program.url, program.checks);
+         var checkJson = checkHtmlFile('csvfile.html', program.checks);
+         // cannot get the following to work, but should work... need to revisit
+         //var checkJson = checkUrlFile(program.url, program.checks);
+      }else{
+         var checkJson = checkHtmlFile(program.file, program.checks);
+      }
     var outJson= JSON.stringify(checkJson,null,4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }   
+ 
